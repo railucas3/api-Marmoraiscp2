8bd12245d5ad196500f496e7780693da3f224929
railucas3 <raimartiniano3@gmail.com>
2026-02-12 20:25:46 -0300

    att2

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,8 +1,17 @@
 import { Router } from 'express'
+import { authenticateToken, isSuperAdmin } from '../middlewares/auth.middleware.js'
+
 const router = Router()
+
+// Aplica proteção para todas as rotas abaixo
+router.use(authenticateToken, isSuperAdmin)
 
 router.get('/', (req, res) => {
   res.json({ module: 'admin' })
+})
+
+router.get('/test', (req, res) => {
+  res.json({ message: 'Bem-vindo SUPER_ADMIN' })
 })
 
 export default router
